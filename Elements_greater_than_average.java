76458defba000199fe617c56af7c7a24e67d2630
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-08-21 21:20:02 +0530

    import java.util.Scanner; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int s=0;         for(int i=0;i<n;i++)         {             s+=a[i];         }         double x=(int)s/n;         int c=0;         for(int i=0;i<n;i++)         {             if(a[i]>=x)             {                c+=1;             }         }         System.out.printf("%d",c);     } }

---
+++ Elements_greater_than_average.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class s{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int s=0;
+        for(int i=0;i<n;i++)
+        {
+            s+=a[i];
+        }
+        double x=(int)s/n;
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>=x)
+            {
+               c+=1; 
+            }
+        }
+        System.out.printf("%d",c);
+    }
+}
