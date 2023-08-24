61f710bb2b4b54499629d8183913ff9c8dd949a2
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-08-24 21:55:47 +0530

    import java.util.Scanner; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int s=0,r=0;         for(int i=0;i<n;i++)         {             if(a[i]%2==0)             {                 s+=a[i];             }         }         System.out.printf("%d",s);     } }

--- Even_Sum.java
+++ Even_Sum.java
@@ -4,12 +4,12 @@ public class s{
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int a[]=new int[n];
+        int[] a=new int[n];
         for(int i=0;i<n;i++)
         {
-           a[i]=sc.nextInt();
+            a[i]=sc.nextInt();
         }
-        int s=0;
+        int s=0,r=0;
         for(int i=0;i<n;i++)
         {
             if(a[i]%2==0)
