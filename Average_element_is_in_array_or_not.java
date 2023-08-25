a7b1f9fe43d3fffb24424d5f9bff93cbff87fb43
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-08-25 22:35:07 +0530

    import java.util.Scanner; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         int s=0;         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();             s+=a[i];         }         int x=s/n;         int f=0;         for(int i=0;i<n;i++)         {             if(x==a[i])             {                 f=1;             }         }         if(f==1)         {             System.out.printf("True");         }         else         {             System.out.printf("False");             }     } }

---
+++ Average_element_is_in_array_or_not.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class s{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        int s=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            s+=a[i];
+        }
+        int x=s/n;
+        int f=0;
+        for(int i=0;i<n;i++)
+        {
+            if(x==a[i])
+            {
+                f=1;
+            }    
+        }
+        if(f==1)
+        {
+            System.out.printf("True");
+        }
+        else
+        {
+            System.out.printf("False");
+            }
+    }
+}
