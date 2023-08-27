98057a1d2d745e72d7631d1a98d0543fa04c1923
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-08-27 19:44:01 +0530

    import java.util.Scanner; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int x=sc.nextInt();         int c=0;         for(int i=0;i<n;i++)         {             if(x==a[i])             {                 c++;             }         }         System.out.printf("%d",c);     } }

---
+++ Count_of_given_element_in_an_array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class s{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int x=sc.nextInt();
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(x==a[i])
+            {
+                c++;
+            }
+        }
+        System.out.printf("%d",c);
+    }
+}
