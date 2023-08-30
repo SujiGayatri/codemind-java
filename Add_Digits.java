0f593e6c9fab6514fa703be4d232810786afa63e
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-08-30 22:32:13 +0530

    import java.util.Scanner; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         System.out.printf("%d",n%9);     } }

--- Add_Digits.java
+++ Add_Digits.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
-public class avrg
-{
+public class s{
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int a;
-        a=sc.nextInt();
-        System.out.printf("%d",a%9);
+        int n=sc.nextInt();
+        System.out.printf("%d",n%9);
     }
 }
