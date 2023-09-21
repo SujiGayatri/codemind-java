c41f9eef99821af4572f0eab2a323447418253a4
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-09-21 20:29:44 +0530

    import java.util.*; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++)         {             int a=sc.nextInt();             int b=sc.nextInt();             int c=0;             for(int j=a;j<=b;j++)             {                 if(j%10==2 || j%10==3 || j%10==9)                 {                     c+=1;                 }
                }
            System.out.printf("%d",c);
            System.out.println();
            }
        }
    }

---
+++ _Counting_Pretty_Numbers.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class s{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=0;
+            for(int j=a;j<=b;j++)
+            {
+                if(j%10==2 || j%10==3 || j%10==9)
+                {
+                    c+=1;
+                }
+                
+            }
+        System.out.printf("%d",c);
+        System.out.println();
+        }
+    }
+}
