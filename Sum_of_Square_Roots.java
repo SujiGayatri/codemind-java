135c4218f800133ba5dab60c17fdbd2eb77ef8c9
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-10-06 14:29:39 +0530

    import java.util.*; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         double s=0,x;         for(int i=a;i<=b;i++)         {             x=Math.sqrt(i);             s+=x;         }         System.out.printf("%.2f",s);     } }

--- Sum_of_Square_Roots.java
+++ Sum_of_Square_Roots.java
@@ -1,18 +1,15 @@
-import java.util.Scanner;
-import java.lang.*;
+import java.util.*;
 public class s{
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
-        double s=0;
-        if(a<b){
+        double s=0,x;
         for(int i=a;i<=b;i++)
         {
-           double x=Math.sqrt(i);
+            x=Math.sqrt(i);
             s+=x;
-        }
         }
         System.out.printf("%.2f",s);
     }
