59395a220e68076c03b3082b3c47ece70efcf8b7
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-10-06 15:20:29 +0530

    import java.util.*; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int r=sc.nextInt();         double x=3.14*r*r;         double y=2*3.14*r;         System.out.printf("%.2f ",x);         System.out.printf("%.2f",y);     } }

---
+++ Area_and_Perimeter_of_Circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class s{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        double x=3.14*r*r;
+        double y=2*3.14*r;
+        System.out.printf("%.2f
+",x);
+        System.out.printf("%.2f",y);
+    }
+}
