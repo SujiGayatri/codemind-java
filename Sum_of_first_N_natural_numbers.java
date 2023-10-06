5dde7e8f7edb4de26b9a4f10cb3db431bd289d93
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-10-06 15:26:50 +0530

    import java.util.*; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int x=(n*(n+1))/2;         System.out.printf("%d",x);     } }

--- Sum_of_first_N_natural_numbers.java
+++ Sum_of_first_N_natural_numbers.java
@@ -1,11 +1,10 @@
-import java.util.Scanner;
-public class sum{
+import java.util.*;
+public class s{
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int n,m;
-        n=sc.nextInt();
-        m=(n*(n+1))/2;
-        System.out.println(m);
+        int n=sc.nextInt();
+        int x=(n*(n+1))/2;
+        System.out.printf("%d",x);
     }
 }
