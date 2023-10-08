8365dd7a4f158faaf94343e3a1d540388d1a41d3
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-10-08 20:03:45 +0530

    import java.util.*; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         if(a%3==2)         {             System.out.printf("SMALL");         }         else if(a%3==0)         {             System.out.printf("NORMAL");         }         else if(a%3==1)         {             System.out.printf("HUGE");         }     } }

--- Mario_and_Mushrooms.java
+++ Mario_and_Mushrooms.java
@@ -3,18 +3,18 @@ public class s{
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        if(n%3==0)
+        int a=sc.nextInt();
+        if(a%3==2)
+        {
+            System.out.printf("SMALL");
+        }
+        else if(a%3==0)
         {
             System.out.printf("NORMAL");
         }
-        else if(n%3==1)
+        else if(a%3==1)
         {
             System.out.printf("HUGE");
-        }
-        else
-        {
-            System.out.printf("SMALL");
         }
     }
 }
