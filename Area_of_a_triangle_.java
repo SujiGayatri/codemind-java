d3f21b2678ecb0770b776f75bcf86984f96a3337
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-10-10 19:39:59 +0530

    import java.util.*; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int b=sc.nextInt();         int h=sc.nextInt();         int x=(b*h)/2;         System.out.printf("%d",x);     } }

--- Area_of_a_triangle_.java
+++ Area_of_a_triangle_.java
@@ -1,13 +1,11 @@
-import java.util.Scanner;
-public class avrg
-{
+import java.util.*;
+public class s{
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int a,b,x;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        x=(a*b)/2;
+        int b=sc.nextInt();
+        int h=sc.nextInt();
+        int x=(b*h)/2;
         System.out.printf("%d",x);
     }
 }
