8a814a56204772e3a380e42efb1d1e94d4e078d9
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-10-10 20:03:04 +0530

    import java.util.*; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n%2!=0)         {             System.out.printf("weird");         }         else if(n%2==0 && 2<=n && n<=5)         {             System.out.printf("not weird");         }         else if(n%2==0 && 6<=n && n<=20)         {             System.out.printf("weird");         }         else if(n%2==0 && n>20)         {             System.out.printf("not weird");         }     } }

--- Weird_or_not_Weird.java
+++ Weird_or_not_Weird.java
@@ -1,17 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
 public class s{
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         if(n%2!=0)
         {
             System.out.printf("weird");
         }
-        else if(n%2==0 && 2<=n && n<5)
+        else if(n%2==0 && 2<=n && n<=5)
         {
             System.out.printf("not weird");
         }
-        else if(n%2==0 && 6<=n && n<20)
+        else if(n%2==0 && 6<=n && n<=20)
         {
             System.out.printf("weird");
         }
@@ -19,5 +20,5 @@ public class s{
         {
             System.out.printf("not weird");
         }
-    } 
+    }
 }
