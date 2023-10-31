97120f4ee5a58ed3819cd2fd5e434a3b7a553ef6
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-10-31 21:10:54 +0530

    import java.util.*; public class s{     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         char a=s.next().charAt(0);         if(a=='a' || a=='e' || a=='i' || a=='o' ||a=='u' || a=='A' || a=='E' || a=='I' || a=='O' || a=='U')         {             System.out.printf("VOWEL");         }         else         {             System.out.printf("CONSONANT");         }     } }

--- VOWEL_or_CONSONANT.java
+++ VOWEL_or_CONSONANT.java
@@ -1,16 +1,16 @@
 import java.util.*;
 public class s{
-public static void main(String[] args)
-{
-    Scanner sc=new Scanner(System.in);
-    char c=sc.next().charAt(0);
-    if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+    public static void main(String[] args)
     {
-        System.out.printf("VOWEL");
+        Scanner s=new Scanner(System.in);
+        char a=s.next().charAt(0);
+        if(a=='a' || a=='e' || a=='i' || a=='o' ||a=='u' || a=='A' || a=='E' || a=='I' || a=='O' || a=='U')
+        {
+            System.out.printf("VOWEL");
+        }
+        else
+        {
+            System.out.printf("CONSONANT");
+        }
     }
-    else
-    {
-        System.out.printf("CONSONANT");
-    }
-}
 }
