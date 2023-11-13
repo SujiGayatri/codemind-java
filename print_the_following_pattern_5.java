2cb38303b1b345cdf2bd20c5bcdb31eb73a310f5
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-11-13 21:15:44 +0530

    import java.util.*; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=n;j++)             {                 if(i==j)                 {                     System.out.printf("0",j);                 }                 else                 {                     System.out.printf("x",j);                 }             }             System.out.println();         }     } }

---
+++ print_the_following_pattern_5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class s{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i==j)
+                {
+                    System.out.printf("0",j);
+                }
+                else
+                {
+                    System.out.printf("x",j);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
