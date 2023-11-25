800dab2b52f67ab2943adc03506b7e206b563b85
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-11-25 22:25:43 +0530

    import java.util.Scanner; public class palin {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,temp=0,rev=0;         n=sc.nextInt();         temp=n;         while(n>0)         {             int d=n%10;             rev=rev*10+d;             n/=10;         }         if(temp==rev)         System.out.print("True");         else         System.out.print("False");     } }

--- Palindrome.java
+++ Palindrome.java
@@ -1,23 +1,21 @@
-import java.util.*;
-public class s{
+import java.util.Scanner;
+public class palin
+{
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int rev=0;
-        int temp=n;
-        while(n!=0)
+        int n,temp=0,rev=0;
+        n=sc.nextInt();
+        temp=n;
+        while(n>0)
         {
-            rev=rev*10+n%10;
-            n=n/10;
+            int d=n%10;
+            rev=rev*10+d;
+            n/=10;
         }
         if(temp==rev)
-        {
-            System.out.println("True");
-        }
+        System.out.print("True");
         else
-        {
-        System.out.println("False");
-        }
+        System.out.print("False");
     }
 }
