4a46309e7c6cc6c33130a9b9021196c674bbe834
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-12-09 22:30:41 +0530

    Add files via upload

---
+++ abstract1.java
@@ -0,0 +1,37 @@
+abstract class Parent
+{
+    abstract void m1();
+    abstract void m2();
+    void m3(){
+        System.out.println("Hello there");
+    }
+}
+class Child1 extends Parent{
+    void m1(){
+        System.out.println("This is method 1");
+    }
+    void m2(){
+        System.out.println("This is method 2");
+    }
+}
+class Child2 extends Parent{
+    void m1(){
+        System.out.println("m1");
+    }
+    void m2(){
+        System.out.println("m2");
+    }
+}
+public class abstract1 {
+    public static void main(String[] args)
+    {
+        Child1 c1 =new Child1();
+        c1.m1();
+        c1.m2();
+        c1.m3();
+        Child2 c2=new Child2();
+        c2.m1();
+        c2.m2();
+        c2.m3();
+    }
+}
