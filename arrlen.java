39af0fbce6d2223eee9f9b9c4e041acf45fa58e4
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-12-10 20:47:21 +0530

    Add files via upload

---
+++ arrlen.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class arrlen{
+              public static void main(String[] args)
+               {
+                         Scanner sc=new Scanner(System.in);
+                         int a[]={10,20,30};
+                         for(int i=0;i<a.length;i++){
+                              System.out.println(a[i]+" ");
+                        }
+               }
+}
