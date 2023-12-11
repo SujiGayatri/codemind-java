b8f2ee31412673f5223717d81d9dd3f55686317e
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-12-11 21:24:34 +0530

    Add files via upload

---
+++ arrmeth.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+public class arrmeth {
+    public static void main(String[] args)
+    {
+        // int[] a={1,2,3,4,5};
+        // int[] b={1,2,3,4,5};
+        // int x=Arrays.compare(a, b);
+        // System.out.println(x);
+        // String[] s={"Po","Shen","Kai"};
+        // Arrays.sort(a,1,6);
+        // //Arrays.sort(s);
+        // System.out.printf(Arrays.toString(a));
+        // //System.out.printf(Arrays.toString(s));
+        int[] a=new int[4];
+        Arrays.fill(a,8);
+        System.out.printf(Arrays.toString(a));
+        //String[] s=new String[3];
+        //Arrays.fill(s,"Suji");
+        //System.out.printf(Arrays.toString(s));
+    }
+}
