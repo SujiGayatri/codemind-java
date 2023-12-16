1ba965be86c69fdaecc00d2949cdce87d933a8c1
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-12-16 21:38:38 +0530

    Add files via upload

---
+++ final.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class final {
+    public static void main(String[] args)
+    {
+        final int a;
+        final double PI=3.14;
+        a=20;
+        b=30;
+        PI=3.15;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
