0516cdaab07de401329fb3d497df0df53ebcfe3c
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-12-17 21:44:50 +0530

    Add files via upload

---
+++ defaultValues.java
@@ -0,0 +1,25 @@
+public class defaultValues {
+
+        static byte b;
+        static short s;
+        static int i;
+        static long l;
+       static   float f;
+        static double d;
+        static char c;
+       static  boolean bo;
+         public static void main(String[] args) {
+     
+             System.out.println("initial value of byte is:"+b);
+             System.out.println("initial value of short is:"+s);
+             System.out.println("initial value of int is:"+i);
+             System.out.println("initial value of long is:"+l);
+             System.out.println("initial value of float is"+f);
+             System.out.println("initial value of double is"+d);
+             System.out.println("initial value of char is:"+c);
+             System.out.println("initial value of boolean is:"+bo);
+     
+         }
+     
+    
+}
