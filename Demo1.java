b363410cec1357781ca53ede763b6401a6adae2f
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-12-18 21:04:10 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Demo1 {
+    public static void main(String[] args)
+    {
+        // Scanner sc=new Scanner(System.in);
+        // int sp,cp;
+        // sp=sc.nextInt();
+        // cp=sc.nextInt();
+        // String r=sp>cp?"PROFIT":"LOSS";
+        // System.out.println(r);
+        int a=23;
+        System.out.println(Integer.toString(a));
+    }
+}
